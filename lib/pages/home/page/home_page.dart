0cd62fe6da4d58013c3f59697104b80ae1506166
shimanta <sarkershimanta@gmail.com>
2024-08-17 21:44:29 +0600

    list view and product view added

--- lib/pages/home/page/home_page.dart
+++ lib/pages/home/page/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shopping_app/utils/image_path/image_path.dart';
 import 'package:shopping_app/widget/support_widget.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,10 +12,10 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List categories = [
-    "images/headphones_icon.png",
-    "images/laptop.png",
-    "images/watch.png",
-    "images/Tv.png",
+    ImagePath.headphone_icon,
+    ImagePath.laptop,
+    ImagePath.watch,
+    ImagePath.tv,
   ];
   @override
   Widget build(BuildContext context) {
@@ -41,7 +43,7 @@ class _HomePageState extends State<HomePage> {
                 ),
                 ClipRRect(
                   borderRadius: BorderRadius.circular(20),
-                  child: Image.asset("images/boy.jpg",
+                  child: Image.asset(ImagePath.boy,
                       height: 80, width: 80, fit: BoxFit.cover),
                 ),
               ],
@@ -71,6 +73,7 @@ class _HomePageState extends State<HomePage> {
               height: 20,
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
                   "Categories",
@@ -86,18 +89,97 @@ class _HomePageState extends State<HomePage> {
                 )
               ],
             ),
-            Container(
-              margin: EdgeInsets.only(left: 20),
-              height: 70,
-              child: ListView.builder(
-                itemCount: categories.length,
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {},
-              ),
-            )
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Container(
+                  height: 130,
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.only(right: 20),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFD6F3E),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "All",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 130,
+                    child: ListView.builder(
+                      itemCount: categories.length,
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) =>
+                          CategoryTile(imagePath: categories[index]),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ListView(
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      Image.asset(
+                        ImagePath.headphone,
+                        height: 60,
+                        width: 60,
+                        fit: BoxFit.cover,
+                      ),
+                      Text(
+                        "Headphone",
+                        style: AppWidget.semiBoldTextFieldStyle,
+                      ),
+                      Text(
+                        "\$100",
+                        style: TextStyle(color: Color(0xFFfd6f3e)),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final String imagePath;
+  const CategoryTile({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.only(right: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        children: [
+          Image.asset(
+            imagePath,
+            height: 50,
+            width: 50,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 10),
+          Icon(Icons.arrow_forward),
+        ],
       ),
     );
   }
